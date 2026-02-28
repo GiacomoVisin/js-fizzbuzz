202350a1ccea51a17522b8d668d388d6d259af33
GiacomoVisin <killerboss1150@gmail.com>
2026-02-28 12:55:29 +0100

    second condition

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ for (let i = 1; i <= 100; i++) {
         console.log(`Fizzbuzz`);
     }
 
-   
+    else if (i % 3 == 0) {
+        console.log(`Fizz`);
+    }
+    else if (i % 5 == 0) {
+        console.log(`Buzz`);
+    }
 }
 
 
