351c53deff30fbdee43ed5c81d3100eb8ba86376
GiacomoVisin <killerboss1150@gmail.com>
2026-02-28 12:54:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/* 
+
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+*/
+
+
+// Inizio creando subito un ciclo for che stampi i numeri da 1 a 100
+// nel ciclo inserisco una condizione che mi seleziona i multipli di 3 stampando Fizz al posto del numero
+// inserisco una seconda condizione che mi seleziona i multipli di 5 stampando Buzz al posto del numero
+// inserisco una terza condizione che mi seleziona i multipli di 3 stampando BuzzFizz al posto del numero
+
+
+
